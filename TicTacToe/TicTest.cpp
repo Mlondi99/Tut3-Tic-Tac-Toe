1f3741602956fe9c3fc97ad7eb3892173a818728
mabhere <213571329@stu.ukzn.ac.za>
2015-03-21 03:14:28 +0200

    almost there! almost

--- TicTacToe/TicTest.cpp
+++ TicTacToe/TicTest.cpp
@@ -3,6 +3,7 @@
 
 #include<iostream>
 using namespace std;
+
 int main()
 {
 	TicTacToe tic;
@@ -10,32 +11,43 @@ int main()
 	cout<< "Welcome,Wamkelekile,Welkom to play the TicTactoe game"<<endl;
 	cout << endl;
 	tic.print();
-	
+	int playtimes = 0;
 	while (1)
 	{
+
 		cout << "enter the field where you want to play" << tic.getPlayer() << ":" << endl;
 		cin >> place;
 		bool isvalid = tic.move(place);
 		if (isvalid == true)
 		{
+			// checking fo winner
+			bool iswon = tic.isWon(playtimes);
+			if (iswon == true)
+			{
+				cout << "Player" << tic.getPlayer() << " Wins!" << endl;
+				Sleep(5000);
+				tic.restart();
+				cout << "New Game. Lets play!" << endl;
+
+			}
+			else if (iswon == false && playtimes == 9)
+			{
+				cout << "It IS DRAW! Let's play again in 5" << endl;
+			}
+			else
+			{
+				cout << " We are still playing. Check! we have " << 9 - playtimes << "moves left" << endl;
+			}
 			tic.changePlayer();
+			playtimes++;
 		}
 		else 
 		{
 			cout << "illegal move! Try again" << endl;
 			continue;
 		}
+		//system("cls");
 		tic.print();
-		// checking fo winner
-		bool iswon = tic.isWon();
-		if (iswon == true)
-		{
-			cout << "Player" << tic.getPlayer() << " Wins!" << endl;
-			Sleep(3000);
-			tic.restart();
-		}
-		else
-			continue;
 		
 	}
 
