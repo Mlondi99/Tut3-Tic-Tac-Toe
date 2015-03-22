043e1e7be72054b9a3617c09711ec8ac6f6609d2
mlondi99 <perfectmadondo@gmail.com>
2015-03-22 09:40:06 +0200

    main

--- TicTacToe/TicTest.cpp
+++ TicTacToe/TicTest.cpp
@@ -11,7 +11,7 @@ int main()
 	cout<< "TicTacToe 1.0"<<endl;
 	cout << endl;
 	tic.print(); 
-	int playtimes = 1;                                                                       // counter to check moves
+	int playtimes = 1;                                                                      // counter to check moves
 	while (1) 
 	{
 
@@ -19,25 +19,25 @@ int main()
 		cin >> place;
 		bool isvalid = tic.move(place);
 		system("cls");
-		if (isvalid == true)                                                                  // if move is true allow player to play
-		{
-			                                                                                  // checking fo winner
+		if (isvalid == true)                                                                 // if move is true allow player to play
+		
+			                                                                                 // checking fo winner
 			bool iswon = tic.isWon(playtimes);
-			if (iswon == true)                                                                //  when one player wins the restart memember will be called
+			if (iswon == true)                                                               //  when one player wins the restart memember will be called
 			{
 				cout << "Player" << tic.getPlayer() << " Wins!" << endl;
-				Sleep(3000);                                                                  // sleep for 5s for the player to see if he has won
+				Sleep(3000);                                                                 // sleep for 5s for the player to see if he has won
 				tic.restart();
-				playtimes = 0;                                                                 // set playtimes back to zero
+				playtimes = 0;                                                               // set playtimes back to zero
 				cout << "New Game. Lets play!" << endl;
 
 			}
 			else if (iswon == false && playtimes == 9)
 			{
-				cout << "the game ended in a DRAW! Let's play again in 5" << endl;
-				Sleep(5000);                                                                    // sleep for 5s for the player to see if it's a draw
+				cout << "the game ended in a DRAW! Let's play the game again in 5" << endl;
+				Sleep(5000);                                                                  // sleep for 5s for the player to see if it's a draw
 				tic.restart(); 
-				playtimes = 0 ;                                                                 // set playtimes back to zero
+				playtimes = 0 ;                                                                // set playtimes back to zero
 				cout << "New Game. Lets play!" << endl;
 			}
 			else
